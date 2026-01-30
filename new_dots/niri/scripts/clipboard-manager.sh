83c1591018ef346ffe16d20e5a23c72499648574
Sunhaloo <azmaan151003@gmail.com>
2026-01-30 22:34:35 +0400

    U: Need to check if we can open applications on specific workspaces
    - Like browser on workspace 3 always
    - Like Disord on workspaces 9 always

---
+++ new_dots/niri/scripts/clipboard-manager.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+cliphist list | rofi -dmenu -p "  " | cliphist decode | wl-copy
---
+++ new_dots/niri/scripts/wallpaper-picker.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# define the wallpaper directory
+WALLPAPER_DIR="$HOME/Wallpapers"
+
+# use rofi to pick out wallpaper
+WALLPAPER=$(find "$WALLPAPER_DIR" -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.jpeg" \) | rofi -dmenu -i -p "Select Wallpaper")
+
+# set the wallpaper using 'swww'
+if [ -n "$WALLPAPER" ]; then
+  swww img "$WALLPAPER" \
+    --transition-type fade \
+    --transition-fps 60 \
+    --transition-duration 2
+fi
