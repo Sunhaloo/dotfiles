93713172e6b353f4202780e53758dbe5182a69de
Sunhaloo <azmaan151003@gmail.com>
2026-06-09 20:52:42 +0400

    U: Added `thorium-browser` to Hyprland keybinds and rules

--- hypr/modules/windowrules.lua
+++ hypr/modules/windowrules.lua
@@ -1,70 +1,77 @@
 -- Docs: https://wiki.hypr.land/Configuring/Basics/Window-Rules/
 
 hl.window_rule({
-    name  = "suppress-maximize-events",
-    match = { class = ".*" },
-    suppress_event = "maximize",
+	name = "suppress-maximize-events",
+	match = { class = ".*" },
+	suppress_event = "maximize",
 })
 
 hl.window_rule({
-    name  = "fix-xwayland-drags",
-    match = {
-        class      = "^$",
-        title      = "^$",
-        xwayland   = true,
-        float      = true,
-        fullscreen = false,
-        pin        = false,
-    },
+	name = "fix-xwayland-drags",
+	match = {
+		class = "^$",
+		title = "^$",
+		xwayland = true,
+		float = true,
+		fullscreen = false,
+		pin = false,
+	},
 
-    no_focus = true,
+	no_focus = true,
 })
 
-
--- Notes 
+-- Notes
 hl.window_rule({
-  name = "notes-windowrule",
-  -- Application: Obsidian
-  match = { class = "obsidian" },
-  workspace = "1",
+	name = "notes-windowrule",
+	-- Application: Obsidian
+	match = { class = "obsidian" },
+	workspace = "1",
 })
 
 -- Terminal
 hl.window_rule({
-  name = "terminal-windowrule",
-  -- Application: Kitty
-  match = { class = "kitty" },
-  workspace = "2",
+	name = "terminal-windowrule",
+	-- Application: Kitty
+	match = { class = "kitty" },
+	workspace = "2",
 })
 
 -- Browser
+-- hl.window_rule({
+-- 	name = "browser-windowrule",
+-- 	-- Application: Zen Browser
+-- 	match = { class = "zen" },
+-- 	workspace = "3",
+-- })
+
+-- Browser
 hl.window_rule({
-  name = "browser-windowrule",
-  -- Application: Zen Browser
-  match = { class = "zen" },
-  workspace = "3",
+	name = "browser-windowrule",
+	-- Application: Thorium Browser
+	match = { class = "Thorium-browser" },
+	workspace = "3",
 })
 
 -- File Manager
 hl.window_rule({
-  name = "filemanager-windowrule",
-  -- Application: Thunar
-  match = { class = "thunar" },
-  workspace = "4",
+	name = "filemanager-windowrule",
+	-- Application: Thunar
+	match = { class = "thunar" },
+	workspace = "4",
 })
 
 -- Internet Messenger
 hl.window_rule({
-  name = "messaging-windowrule",
-  -- Application: Discord
-  match = { class = "discord" },
-  workspace = "9",
+	name = "messaging-windowrule",
+	-- Application: Discord
+	match = { class = "discord" },
+	workspace = "9",
 })
 
 -- Recording
 hl.window_rule({
-  name = "recording-windowrule",
-  -- Application: OBS Studio
-  match = { class = "com.obsproject.Studio" },
-  workspace = "10",
+	name = "recording-windowrule",
+	-- Application: OBS Studio
+	match = { class = "com.obsproject.Studio" },
+	workspace = "10",
 })
--- hypr/modules/workspacerules.lua
+++ hypr/modules/workspacerules.lua
@@ -1,14 +1,19 @@
 -- Docs: https://wiki.hypr.land/Configuring/Basics/Workspace-Rules/
 
--- NOTE: just test it out and see how it feels like
--- This is going to make workspace 2 ( only ) to have a scrolling behaviour
+-- Workspace 3
 hl.workspace_rule({
-  workspace = "2",
-  layout = "scrolling"
+	workspace = "2",
+	layout = "scrolling",
+})
+
+-- Workspace 3
+hl.workspace_rule({
+	workspace = "3",
+	layout = "scrolling",
 })
 
 -- Workspace 7
 hl.workspace_rule({
-  workspace = "7",
-  gaps_out = 25,
+	workspace = "7",
+	gaps_out = 25,
 })
